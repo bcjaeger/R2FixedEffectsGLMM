8bc75466ac0972ae8c8d1820986ea6c0893888b6
Byron <byron.jaeger@gmail.com>
2015-11-05 09:17:17 -0500

    Dental Data Simulation
    This code creates simulated dental data and shows that the r squared
    statistic proposed by Nakagawa and Schielzeth is a special case of my
    proposed r squared statistic

--- R2_Dental_Dat_Sim_BCJ_03NOV2015.R
+++ R2_Dental_Dat_Sim_BCJ_03NOV2015.R
@@ -7,9 +7,116 @@ library(nlme)
 library(MASS)
 library(plyr)
 library(dplyr)
+library(tables)
 
-# Load my babies
-source('My_Functions.R')
+# Load all the functions
+
+r2beta <- function(model = NULL, partial = F){
+  
+  # Computes R^2 statistic from edwards et al., 2008.
+  #
+  # Args:
+  #    model: A linear mixed model fit with the lmer() function
+  #    partial: if TRUE, includes computation of partial R2
+  #             for all fixed effects in the model
+  #
+  # Returns:
+  #   a dataframe containing the model F statistic, numerator  
+  #   and denominator degrees of freedom, and R^2 statistic. If
+  #   partial = TRUE, then the dataframe also contains partial
+  #   R^2 statistics for all fixed effects in the model
+  # 
+  # Programmer:  
+  #   Byron C. Jaeger
+  #   bcjaeger@live.unc.edu
+  
+  require(lme4)
+  require(pbkrtest) 
+  require(afex)
+  require(dplyr)
+  
+  ### Get random effects from model call
+  
+  random = paste('(',unlist(lme4::findbars(model@call)), ')', sep = '', collapse = '+')
+  
+  ### Null model formula: 
+  ### exactly the same covariance structure with all fixed effects removed except the intercept 
+  
+  null.form <- as.formula(paste('. ~ 1 +', random))
+  
+  ### Compute Kenward Roger approximate F test using the null model defined above
+  
+  mc <- pbkrtest::KRmodcomp(model, update(model, null.form))$stat 
+  
+  ### Compute the R2beta statistic for the full model
+  ### Store results in a dataframe r2
+  
+  R2 = data.frame(Effect = 'Model', F = mc$Fstat, v1 = mc$ndf, v2 = mc$ddf, ncp = mc$Fstat * mc$ndf,
+                  Rsq       = with(mc, (ndf * Fstat  / ddf   ) / (1 + ndf * Fstat  / ddf  )))
+  
+  ### use mixed function to conduct approximate KR F tests for each
+  ### fixed effect in the full model
+  
+  if (partial == T){
+    
+    partials <- afex::mixed(model@call$formula, data = model@frame, progress = F)$anova.table
+    
+    ### Compute partial R2beta statistics for all fixed effects
+    
+    r2part = mutate(data.frame(partials[c('Effect', 'F', 'ndf', 'ddf')]), 
+                    Rsq = (ndf * F / ddf) / (1 + ndf*F/ddf), 
+                    ncp = F * ndf) %>% rename(v1 = ndf, v2 = ddf)
+    
+    R2 = rbind(R2, r2part) 
+    
+  }
+  
+  return(R2)
+  
+}
+r2beta.NS <- function(model = m, slope = Orthodont$age){
+  
+  # This function calculates a simplified version of R2_Beta 
+  # using the Wald F statistic from a linear mixed model.
+  # The resulting R^2 statistic is approximately equivalent
+  # to the marginal R^2 proposed by Nakagawa and Schielzeth (2014) 
+  
+  # Note: this function is sloppily coded and only works properly
+  # for the dental simulation study. 
+  
+  beta = fixef(model)
+  X = model.matrix(model)
+  n <- nrow(X)
+  if (ncol(VarCorr(model)[1]$Subject) > 1){Z = cbind(1, slope)}
+  else {Z = matrix(1, nrow = n)}
+  p <- length(beta)
+  C = cbind(rep(0, p-1),diag(p-1))
+  
+  # Actual Covariance Matrix from the model
+  SigHat = Z%*%VarCorr(model)[1]$Subject%*%t(Z) + sigma(model)^2*diag(nrow(Z))
+  
+  # NS mean of the trace of the matrix above
+  sighat = mean(diag(SigHat))
+  
+  # Wald F stat using the NS matrix
+  WaldF = t(C %*% beta) %*% ginv(C %*% solve(t(X)%*%X * (sighat)^(-1)) %*% t(C)) %*% C%*%beta / (ncol(C)-1)
+  
+  # Fstat using the real matrix
+  WaldF.r = t(C %*% beta) %*% ginv(C %*% solve(t(X)%*%solve(SigHat)%*%X ) %*% t(C)) %*% C%*%beta / (ncol(C)-1)
+  
+  ddf = n-p
+  ndf = p-1
+  ss = ndf/ddf * WaldF
+  
+  R2 = ss/(1+ss) 
+  
+  return(R2)
+}
+
+# Some functions for tables
+myfun=function(x){c(Mean = mean(x), Sd = sd(x))} 
+tabfun <- function(x,y, dig=2){paste(round(x,dig), ' (', round(y, dig), ')', sep = '')} 
+
 
 # Set the number of sims to run
 nsims = 10000
@@ -23,7 +130,7 @@ data(Orthodont)
 # Make three matrices to hold results for the three outcome types
 # R2.Norm1 holds the R2 values from the correct model
 # R2.Norm2 holds R2 values from an underspecified model
-# R2.Norm3 holds R2 values from a misspecified model
+# R2.Norm3 holds R2 values from model with noise parameters
 
 R2.Norm1 = R2.Norm2 = R2.Norm3 = matrix(0, nrow = nsims, ncol = length(nms), dimnames = list(1:nsims,  nms))
 Tabs = R2.Dat = list()
@@ -61,16 +168,13 @@ for (sim in 1:nsims){
   # Model 3 is a misspecified model
   m3 <- update(m0, .~. + noise*grp)
              
-  R2.Norm1[sim, ] = unlist(c(r.squaredGLMM(m1), r2beta.NS(m1), r2beta.glmm(m1, partial = F)[c('Rsq')]))
-  R2.Norm2[sim, ] = unlist(c(r.squaredGLMM(m2), r2beta.NS(m2), r2beta.glmm(m2, partial = F)[c('Rsq')]))
-  R2.Norm3[sim, ] = unlist(c(r.squaredGLMM(m3), r2beta.NS(m3), r2beta.glmm(m3, partial = F)[c('Rsq')]))
+  R2.Norm1[sim, ] = unlist(c(r.squaredGLMM(m1), r2beta.NS(m1), r2beta(m1, partial = F)[c('Rsq')]))
+  R2.Norm2[sim, ] = unlist(c(r.squaredGLMM(m2), r2beta.NS(m2), r2beta(m2, partial = F)[c('Rsq')]))
+  R2.Norm3[sim, ] = unlist(c(r.squaredGLMM(m3), r2beta.NS(m3), r2beta(m3, partial = F)[c('Rsq')]))
   
 }
 
-# Look at summary stats for each matrix
-myfun=function(x){c(Mean = mean(x), Sd = sd(x))}
-tabfun <- function(x,y, dig=2){paste(round(x,dig), ' (', round(y, dig), ')', sep = '')}
-
+# Get summary statistics
 r2.norm1 = data.frame(t(apply(R2.Norm1, 2, myfun))) %>% mutate(Model = 'Full', Statistic = colnames(R2.Norm1))
 r2.norm2 = data.frame(t(apply(R2.Norm2, 2, myfun))) %>% mutate(Model = 'Reduced', Statistic = colnames(R2.Norm2))
 r2.norm3 = data.frame(t(apply(R2.Norm3, 2, myfun))) %>% mutate(Model = 'Noise', Statistic = colnames(R2.Norm3))
@@ -81,42 +185,21 @@ r2.df <- rbind(r2.norm1, r2.norm2, r2.norm3) %>%
                 tabvar = tabfun(Mean, Sd)) %>%
   plyr::arrange(Model, Statistic)
 
-library(tables)
-
 R2.Dat[[cov]] = r2.df
 Tabs[[cov]] = tabular(Model ~ Statistic * Heading() * (tabvar * Heading()*paste), data = r2.df)
 
 }
 
-gdat = rbind(R2.Dat$`Covariance 1` %>% mutate(Cov = 'Covariance 1'),
-             R2.Dat$`Covariance 2` %>% mutate(Cov = 'Covariance 2')) %>% 
-  dplyr::select(-Mean, -Sd) %>% reshape2::melt(value.name = 'tabvar')
+rbind(Tabs[[1]], Tabs[[2]])
 
-latex(rbind(Tabs[[1]], Tabs[[2]]))
-
-# Graphs
-
-gdat <- reshape2::melt(gdat, value.name = 'R2')
-gdat <- dplyr::rename(gdat, Statistic = variable)
-
-ggplot(gdat, aes(x=R2, fill=Statistic)) +
-  geom_histogram(binwidth=0.01, colour="white", position = position_stack())
-
-r2sim.plot(r2.df)
-
-
-
-# y is the response variable
-y = Orthodont$distance
+# Some scratchwork used to show that R^2_beta reduces to R^2_NS in the normal case
 
 m0 <- lmer(distance~1+(1+age|Subject), data = Orthodont)
-m00 <- lm(distance~1, data = Orthodont)
+m <- lmer(distance~age+(1+age|Subject), data = Orthodont)
 mm=summary(m); mm0 = summary(m0)
 
-m <- lmer(distance~age+(1+age|Subject), data = Orthodont)
-
+# Check to make sure my r2beta.NS function agrees with Naka/Schielz
 r2beta.NS(m)
-
 r.squaredGLMM(m)
 
 # pred is the predicted values of y from the model m
@@ -124,33 +207,20 @@ r.squaredGLMM(m)
 pred = model.matrix(m)%*%fixef(m)
 pred0 = model.matrix(m0)%*%fixef(m0)
 
+# y is the response variable
+y = Orthodont$distance
+
 # sigma^2 from the full model
 s2 = var(y - pred)
- (t(y) %*% y - t(y)%*%M%*% y) / (nrow(Orthodont)-1)
 
 # sigma^2 from the null model
 s20 = var(y - pred0)
-( t(y) %*% y - t(y)%*%M0%*% y) / (nrow(Orthodont)-1)
 
 # This is sigma^2_f
 s20 - s2
+# Variance of fixed effects (Snijders and Bosker, 2012)
+s2f = var(pred) 
 
-s2f = var(pred) # Variance of fixed effects (Snijders and Bosker, 2012)
-s2e = mm$sigma^2 # Variance of residual error
-s2b0 = mm$varcor$Subject[1,1] # variance of random intercept
-s2b1 = mm$varcor$Subject[2,2] # variance of random slopes for age
-s2b01 = mm$varcor$Subject[2,1] # covariance of intercept and clope
-
-# This is what Naka/Schielz do  
-# Calculate sigma^2_l, the variance explained by random effects
-s2l = s2b0 + mean(Orthodont$age^2*s2b1 + 2*Orthodont$age*s2b01)
-# Marginal R2
-s2f / (s2f + s2e + s2l)
-# Conditional R2
-(s2f + s2l) / (s2f + s2e + s2l)
-
-# The MLE for s2e from a lm is close to s2l + s2e
-summary(lm(distance ~ age*Sex, data = Orthodont))$sigma^2
 
 
 

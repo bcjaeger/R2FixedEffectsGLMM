16b358bdc2243bbca5d73c56cda88d28e24e5135
Byron <byron.jaeger@gmail.com>
2016-04-20 11:45:09 -0400

    Update r2betaNS_Function.R

--- r2betaNS_Function.R
+++ r2betaNS_Function.R
@@ -1,41 +1,50 @@
 
 
-r2beta.NS <- function(model = m, slope = orthodont$age){
+r2beta.NS <- function(model){
+  
+  # model: an lmer object
+  # Note: As of now, this function only works for 2 level models
   
   # This function calculates a simplified version of R2_Beta 
-  # using the Wald F statistic from a linear mixed model.
+  # using the average variance from \hat{\Sigma}.
   # The resulting R^2 statistic is approximately equivalent
   # to the marginal R^2 proposed by Nakagawa and Schielzeth (2014) 
   
-  # Note: this function is sloppily coded and only works properly
-  # for the dental simulation study. 
-  
+  # Get fixed effects
   beta = fixef(model)
+  p <- length(beta)
+  
+  # Get random effects
+  Sig.b = raneff.covmat(model)
+  
+  # Get model matrices
   X = model.matrix(model)
   n <- nrow(X)
-  if (ncol(VarCorr(model)[1]$Subject) > 1){Z = cbind(1, slope)}
-  else {Z = matrix(1, nrow = n)}
-  p <- length(beta)
+  Z = getME(model, 'Z')
+  
+  # C matrix defines the Wald Test for Fixed Effects
   C = cbind(rep(0, p-1),diag(p-1))
   
-  # Actual Covariance Matrix from the model
-  SigHat = Z%*%VarCorr(model)[1]$Subject%*%t(Z) + sigma(model)^2*diag(nrow(Z))
+  # Counts of level 1 units within level 2 
+  N = as.numeric(table(eval(model@call$data)[, names(model@flist)[1]]))
+
+  # Stacked (diagonal) random effects covariance matrix
+  Sig.B = kronecker(diag(length(N)), Sig.b)
   
-  # NS mean of the trace of the matrix above
+  # Covariance Matrix from the model
+  SigHat = Z%*%Sig.B%*%t(Z) + sigma(model)^2*diag(nrow(Z))
+  
+  # Mean of the trace of the matrix above
   sighat = mean(diag(SigHat))
   
-  # Wald F stat using the NS matrix
+  # Wald F stat using the mean covariance estimate, 
   WaldF = t(C %*% beta) %*% ginv(C %*% solve(t(X)%*%X * (sighat)^(-1)) %*% t(C)) %*% C%*%beta / (ncol(C)-1)
   
-  # Fstat using the real matrix
-  WaldF.r = t(C %*% beta) %*% ginv(C %*% solve(t(X)%*%solve(SigHat)%*%X ) %*% t(C)) %*% C%*%beta / (ncol(C)-1)
-  
-  ddf = n-p
-  ndf = p-1
+  ddf = n-1
+  ndf = ncol(C)-1
   ss = ndf/ddf * WaldF
-  
   R2 = ss/(1+ss) 
   
   return(R2)
+   
 }
-

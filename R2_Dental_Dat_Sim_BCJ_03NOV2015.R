1c95d176e95f67f63babbb192920de77212ebe92
Byron <byron.jaeger@gmail.com>
2016-05-22 10:02:01 -0400

    Update R2_Dental_Dat_Sim_BCJ_03NOV2015.R

--- R2_Dental_Dat_Sim_BCJ_03NOV2015.R
+++ R2_Dental_Dat_Sim_BCJ_03NOV2015.R
@@ -1,5 +1,4 @@
 
-
 library(lme4)
 library(MuMIn)
 library(ggplot2)
@@ -8,6 +7,8 @@ library(MASS)
 library(plyr)
 library(dplyr)
 library(tables)
+
+# Note: r2beta and r2beta.NS functions are needed to run the simulation.
 
 # Some functions for tables
 myfun=function(x){c(Mean = mean(x), Sd = sd(x))} 
@@ -37,63 +38,54 @@ subs = unique(Orthodont$Subject)
 n = length(subs)
 
 for(cov in c('Covariance 1', 'Covariance 2')){
-
+  
   model.cov = ifelse(cov == 'Covariance 1', '(1|Subject)', '(1+age|Subject)')
   null.form = paste('sim_1 ~ 1 +', model.cov)  
-
-# Dental data simulation
-for (sim in 1:nsims){
-
-  # Something to watch
-  # print(sim)
   
-  # Generate the data by simulating data from m
-  dat = cbind(Orthodont,simulate(m, 1, seed = sim))
-  dat = merge(dat, data.frame(Subject = subs, grp = rbinom(length(subs), 1, 0.5)))
-  dat$noise = rnorm(nrow(dat))
+  # Dental data simulation
+  for (sim in 1:nsims){
+    
+    # Something to watch
+    # print(sim)
+    
+    # Generate the data by simulating data from m
+    dat = cbind(Orthodont,simulate(m, 1, seed = sim))
+    dat = merge(dat, data.frame(Subject = subs, grp = rbinom(length(subs), 1, 0.5)))
+    dat$noise = rnorm(nrow(dat))
+    
+    # Model 0 is the null model
+    m0 <- lmer(as.formula(null.form), data = dat)
+    
+    # Model 1 is the correct model '
+    m1 <- update(m0, .~. + age*Sex)
+    
+    # Model 2 is an underspecified model
+    m2 <- update(m0, .~. + age)
+    
+    # Model 3 is a misspecified model
+    m3 <- update(m0, .~. + noise*grp)
+    
+    R2.Norm1[sim, ] = unlist(c(r.squaredGLMM(m1), r2beta.NS(m1), r2beta(m1, partial = F)[c('Rsq')]))
+    R2.Norm2[sim, ] = unlist(c(r.squaredGLMM(m2), r2beta.NS(m2), r2beta(m2, partial = F)[c('Rsq')]))
+    R2.Norm3[sim, ] = unlist(c(r.squaredGLMM(m3), r2beta.NS(m3), r2beta(m3, partial = F)[c('Rsq')]))
+    
+  }
   
-  # Model 0 is the null model
-  m0 <- lmer(as.formula(null.form), data = dat)
-
-  # Model 1 is the correct model '
-  m1 <- update(m0, .~. + age*Sex)
-
-  # Model 2 is an underspecified model
-  m2 <- update(m0, .~. + age)
+  # Get summary statistics
+  r2.norm1 = data.frame(t(apply(R2.Norm1, 2, myfun))) %>% mutate(Model = 'Full', Statistic = colnames(R2.Norm1))
+  r2.norm2 = data.frame(t(apply(R2.Norm2, 2, myfun))) %>% mutate(Model = 'Reduced', Statistic = colnames(R2.Norm2))
+  r2.norm3 = data.frame(t(apply(R2.Norm3, 2, myfun))) %>% mutate(Model = 'Noise', Statistic = colnames(R2.Norm3))
   
-  # Model 3 is a misspecified model
-  m3 <- update(m0, .~. + noise*grp)
-             
-  R2.Norm1[sim, ] = unlist(c(r.squaredGLMM(m1), r2beta.NS(m1), r2beta(m1, partial = F)[c('Rsq')]))
-  R2.Norm2[sim, ] = unlist(c(r.squaredGLMM(m2), r2beta.NS(m2), r2beta(m2, partial = F)[c('Rsq')]))
-  R2.Norm3[sim, ] = unlist(c(r.squaredGLMM(m3), r2beta.NS(m3), r2beta(m3, partial = F)[c('Rsq')]))
+  r2.df <- rbind(r2.norm1, r2.norm2, r2.norm3) %>% 
+    dplyr::mutate(Model = factor(Model, levels = c('Full', 'Reduced', 'Noise')),
+                  Statistic = factor(Statistic),
+                  tabvar = tabfun(Mean, Sd)) %>%
+    plyr::arrange(Model, Statistic)
   
-}
-
-# Get summary statistics
-r2.norm1 = data.frame(t(apply(R2.Norm1, 2, myfun))) %>% mutate(Model = 'Full', Statistic = colnames(R2.Norm1))
-r2.norm2 = data.frame(t(apply(R2.Norm2, 2, myfun))) %>% mutate(Model = 'Reduced', Statistic = colnames(R2.Norm2))
-r2.norm3 = data.frame(t(apply(R2.Norm3, 2, myfun))) %>% mutate(Model = 'Noise', Statistic = colnames(R2.Norm3))
-
-r2.df <- rbind(r2.norm1, r2.norm2, r2.norm3) %>% 
-  dplyr::mutate(Model = factor(Model, levels = c('Full', 'Reduced', 'Noise')),
-                Statistic = factor(Statistic),
-                tabvar = tabfun(Mean, Sd)) %>%
-  plyr::arrange(Model, Statistic)
-
-R2.Dat[[cov]] = r2.df
-Tabs[[cov]] = tabular(Model ~ Statistic * Heading() * (tabvar * Heading()*paste), data = r2.df)
-
+  r2.df$Cov = factor(cov)
+  R2.Dat[[cov]] = r2.df
+  Tabs[[cov]] = tabular(Cov * Model ~ Statistic * Heading() * (tabvar * Heading()*paste), data = r2.df)
+  
 }
 
 rbind(Tabs[[1]], Tabs[[2]])
-
-
-
-
-
-
-
-
-
-
